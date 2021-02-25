89e1e65959721af2a374e7c549d48e7aac497c02
Valerie Ren <zhaorui.ren@pm.me>
2021-02-25 22:44:22 +0100

    added image zoom in/out function

--- src/components/ImageViewer.js
+++ src/components/ImageViewer.js
@@ -1,10 +1,20 @@
 import React, {useEffect, useState} from "react";
 import "./ImageViewer.css";
 import * as ImageAction from "../actions";
+import {getHDImageUrl} from "../utils/helper";
 
 const ImageViewer = ({ imagePath }) => {
   const [imgIndex, setImgIndex] = useState(0);
   const [totalImages, setTotalImages] = useState(1);
+  const [showZoomedImg, setShowZoomedImg] = useState(false);
+  const [crop, setCrop] = useState({
+    x: 0,
+    y: 0,
+    imgX: 0,
+    imgY: 0,
+    width: 512,
+    height: 512,
+  });
   const [dragState, setDragState] = useState({
     dragging: false,
     dragStart: 0,
@@ -38,6 +48,13 @@ const ImageViewer = ({ imagePath }) => {
 
   const onMouseMove = (e) => {
     e.preventDefault();
+    if (showZoomedImg) {
+      setCrop({
+        ...crop,
+        x: e.pageX - crop.imgX,
+        y: e.pageY - crop.imgY,
+      });
+    }
     if (!dragState.dragging) return;
     const currentPosition = e.screenX;
     const widthPerImage = 360 / totalImages;
@@ -58,15 +75,46 @@ const ImageViewer = ({ imagePath }) => {
     setDragState({...dragState, dragging: false});
   }
 
+  const onClick = (e) => {
+    e.preventDefault();
+    if (dragState.dragging) return;
+    setShowZoomedImg(!showZoomedImg);
+    if (showZoomedImg) {
+      setCrop({
+        ...crop,
+        x: e.pageX - crop.imgX,
+        y: e.pageY - crop.imgY,
+      });
+    }
+  };
+
   return (
     <div className="image-view-container">
       <img
         className="image-360"
-        onMouseDown={onMouseDown}
+        onClick={onClick}
+        onDragStart={onMouseDown}
         onMouseMove={onMouseMove}
         onMouseUp={onMouseUp}
-        src={ImageAction.getImageImportByIndex(imagePath, imgIndex + 1)}
-      />
+        onLoad={e => {
+          // loaded the image
+          window.dispatchEvent(new Event('resize'));
+          // get the img height and width
+          setCrop({
+            ...crop,
+            imgX: e.currentTarget.x,
+            imgY: e.currentTarget.y,
+            width: e.currentTarget.width,
+            height: e.currentTarget.height,
+          })
+        }}
+        draggable={!showZoomedImg}
+        // the src image will be shown determined if the function is for zoom or for 360 view
+        src={showZoomedImg ?
+          `${getHDImageUrl(imagePath, imgIndex + 1, crop.x, crop.y, crop.width, crop.height, "1k")}` :
+          ImageAction.getImageImportByIndex(imagePath, imgIndex + 1)
+        }
+        alt="" />
     </div>
   );
 };
